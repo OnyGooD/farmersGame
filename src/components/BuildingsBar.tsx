c044c7fdf7cdea7547ec9f568045d6be906142ff
OnyGooD <molnekkrisz@gmail.com>
2026-09-22 11:04:36 +0200

    add useResourceStore

---
+++ src/components/BuildingsBar.tsx
@@ -0,0 +1,7 @@
+const BuildingsBar = () => {
+    return (
+        <div>BuildingsBar</div>
+    )
+}
+
+export default BuildingsBar
---
+++ src/store/useResourceStore.ts
@@ -0,0 +1,40 @@
+import { create } from "zustand";
+
+type ResourceType = "wood" | "stone" | "food"
+
+type ResourceStoreType = {
+    gold: number;
+    wood: number;
+    stone: number;
+    food: number;
+
+    spendGold: (amount: number) => void,
+    receiveGold: (amount: number) => void,
+
+    addResource: (type: ResourceType, amount: number) => void,
+    spendResource: (type: ResourceType, amount: number) => boolean,
+}
+
+
+export const useResourceStore = create<ResourceStoreType>((set) => ({
+    gold: 1000,
+    wood: 0,
+    stone: 0,
+    food: 0,
+    spendGold: (amount: number) => {
+        if (amount > 0 && amount <= useResourceStore.getState().gold) {
+            set((state) => ({ gold: state.gold - amount }))
+            return true;
+        }
+        return false;
+    },
+    receiveGold: (amount: number) => { set((state) => ({ gold: state.gold + amount })) },
+    addResource: (type: ResourceType, amount: number) => { set((state) => ({ [type]: state[type] + amount })) },
+    spendResource: (type: ResourceType, amount: number) => {
+        if (amount > 0 && amount <= useResourceStore.getState().gold) {
+            set((state) => ({ [type]: state[type] - amount }))
+            return true;
+        }
+        return false;
+    }
+}))
